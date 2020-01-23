417bba552d52cc10213998eebda78241a7b5108d
Swati Arora <arora.swati@snapdeal.com>
2020-01-23 18:39:12 +0530

    Map Container added in route

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import MapContainer from "./MapContainer";
-
-//ReactDOM.render(<MapContainer />, document.getElementById("root"));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
